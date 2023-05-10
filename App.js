cfb7909c43e6ce1e49da5be9a1ac256aa5ac5cec
Mikhael <116017125+mikhaelnice@users.noreply.github.com>
2023-05-10 07:24:08 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+import EnterPin from "./pages/EnterPin";
+import ResetPin from "./pages/ResetPin";
+import ResetOptPin from "./pages/ResetOptPin";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <ResetOptPin />
+      <Footer />
+    </>
+  );
+}
+export default App;
